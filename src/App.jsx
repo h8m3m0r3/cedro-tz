23765e889bcfcc89983c0879deecc6081e252795
h8m3m0r3 <danilayumatov@yandex.ru>
2023-10-23 04:05:12 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import 'simplebar/dist/simplebar.css';
 import searchIcon from "./assets/images/searchIcon.svg";
 import deleteIcon from "./assets/images/deleteIcon.svg";
 
@@ -51,8 +53,13 @@ function App() {
       <div className="card">
         <header className="card__header">Локация</header>
         <button className="card__btn" onClick={() => setIsOpen(!isOpen)}>
-          <img src={searchIcon} alt="Иконка поиска" />
-          <p className="btn__text">ЖК, Округ, район, метро</p>
+          <section className="btn__info">
+            <img src={searchIcon} alt="Иконка поиска" />
+            <p className="btn__text">ЖК, Округ, район, метро</p>
+          </section>
+          {pickedItems.length > 0 && (
+            <span className="card__btn counter">{pickedItems.length}</span>
+          )}
         </button>
         <div
           className={`dropdown ${isOpen ? "active" : ""}`}
@@ -88,48 +95,34 @@ function App() {
               метро
             </li>
           </ul>
-          {/* <div className="picked__itemsss">
-
-          </div> */}
-          <ul
-            className="picked__items"
-            style={{
-              display: pickedItems.length === 0 ? "none" : "flex",
-              height: "58px",
-            }}
+          <div
+            className={`picked__items ${pickedItems.length === 0 ? "" : "visible"}`}
+            
           >
-            {pickedItems.map((item) => (
-              <li key={item.id} className="picked__item">
-                <p>{item.name}</p>
-                <img
-                  src={deleteIcon}
-                  alt="Удалить"
-                  className="delete-icon"
-                  onClick={() => handleDeleteItem(item.id)}
-                />
-              </li>
-            ))}
-          </ul>
-          <div className="tab__list">
-            <ul>
+            <ul className="picked__list">
+              {pickedItems.map((item) => (
+                <li key={item.id} className="picked__item">
+                  <p>{item.name}</p>
+                  <img
+                    src={deleteIcon}
+                    alt="Удалить"
+                    className="delete-icon"
+                    onClick={() => handleDeleteItem(item.id)}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tab__list" data-simplebar>
+            <ul className="tab__items">
               {Object.keys(locationList[category]).map((item) => {
                 return (
                   <li key={locationList[category][`${item}`].id}>
                     <p>{locationList[category][`${item}`].name}</p>
-                    <div className="custom__checkbox">
-                      <input
-                        type="checkbox"
-                        className="tab__item checkbox"
-                        id={`myCheckbox${locationList[category][`${item}`].id}`}
-                        checked={locationList[category][item].picked}
-                        onChange={() => handleCheckboxChange(item)}
-                      />
-                      <label
-                        htmlFor={`myCheckbox${
-                          locationList[category][`${item}`].id
-                        }`}
-                      ></label>
-                    </div>
+                    <button className={`custom__checkbox ${locationList[category][item].picked ? "checked" : ""}`}
+                    checked={locationList[category][item].picked}
+                    onClick={() => handleCheckboxChange(item)}>
+                    </button>
                   </li>
                 );
               })}
